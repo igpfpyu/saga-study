22caa8e96976012430110a451a1f44d5f0edd1af
iw <igpfpyu@vip.qq.com>
2019-06-08 20:14:35 +0800

    info

--- src/claim/pages/components/Home_action.js
+++ src/claim/pages/components/Home_action.js
@@ -20,6 +20,10 @@ export function* fetchPerson(action){
 }
 export function* fetchPlanets(action){
     try {
+        //put(action); - 发起一个action 到store;
+        //action：Object;
+        //put是异步的，不会立即发生。
+
         yield put({
             type:types.FETCH_SATR_WARS_PLANET_SUCCESS,
             data:["x",'y','z']
@@ -32,3 +36,14 @@ export default function* saga(){
     yield takeEvery(types.FETCH_SATR_WARS_REQURES, fetchPerson);
     yield takeEvery(types.FETCH_SATR_WARS_PLANET_REQURES, fetchPlanets)
 }
+
+
+//call(fn, ...args)阻塞执行，call执行完，才会往下执行；
+//fn：Function - 一个Generator函数，或者返回Promise的普通函数；
+//args：Array，一个数组，作为fn的参数；
+//yield[call(fetch, '/users'), call()s ];
+
+//race(effects)
+//effects : Object - 一个{label:effect, ...}形式的字典对象；
+
+
