e55ca3ec90adb1fc4d6d5a067a05efc5e1fdee2b
iw <igpfpyu@vip.qq.com>
2019-06-11 09:36:37 +0800

    info

--- src/claim/pages/components/Home_action.js
+++ src/claim/pages/components/Home_action.js
@@ -20,6 +20,7 @@ export function* fetchPerson(action){
 }
 export function* fetchPlanets(action){
     try {
+        // const data = yield call(Api.fetchUser, action.payload.url);
         //put(action); - 发起一个action 到store;
         //action：Object;
         //put是异步的，不会立即发生。
@@ -32,7 +33,10 @@ export function* fetchPlanets(action){
         console.log(e)
     }
 }
-export default function* saga(){
+export default function* saga(action){
+    //takeEvery传入一个action类型和对应要执行的异步操作方法。
+    //捕捉action执行异步函数；
+    //在发起（dispatch）到 Store 并且匹配 pattern 的每一个 action 上派生一个 saga
     yield takeEvery(types.FETCH_SATR_WARS_REQURES, fetchPerson);
     yield takeEvery(types.FETCH_SATR_WARS_PLANET_REQURES, fetchPlanets)
 }
--- src/claim/redux/store.js
+++ src/claim/redux/store.js
@@ -6,12 +6,14 @@ import mySaga from './sagas';
 // - 参数是一个数组，里面是generator函数列表。
 // - 创建一个redux中间件，并将sagas和redux store建立链接；
 const sagaMiddleware=createSagaMiddleware();
+
 export default createStore(
     reducer,
-    applyMiddleware(sagaMiddleware)
+    applyMiddleware(...sagaMiddleware)
 );
 //动态执行saga, 用于applyMiddleware阶段之后执行sagas.返回一个task描述对象；
 //sagaMiddleware.run(saga, ...args)
 //saga:function 一个generator函数；
 //args：Array提供给saga的参数（除了store的getState方法）；
 sagaMiddleware.run(mySaga);
+
