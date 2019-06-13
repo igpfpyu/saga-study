a694dcdd34593581493e1b9c8fb13a2b7c38be6f
iw <igpfpyu@vip.qq.com>
2019-06-13 21:23:39 +0800

    info

--- src/claim/pages/components/Home_reducer.js
+++ src/claim/pages/components/Home_reducer.js
@@ -3,23 +3,38 @@ const initialState={
     people:[],
     planet:[],
 }
-const handleStarWarsFetchSuccess=(state, action)=>{
-    return {
-        ...state,
-        people:action.data
+// const handleStarWarsFetchSuccess=(state, action)=>{
+//     return {
+//         ...state,
+//         people:action.data
+//     }
+// }
+// const handleStarWarsPlanetFetchSuccess=(state, action)=>{
+//     console.log(action.data);
+//     return {
+//         ...state,
+//         planet: action.data
+//     }
+// }
+export default(state={}, action)=>{
+    // const handlers={
+    //     [types.FETCH_SATR_WARS_SUCCESS]:handleStarWarsFetchSuccess,
+    //     [types.FETCH_SATR_WARS_PLANET_SUCCESS]:handleStarWarsPlanetFetchSuccess
+    // }
+    // return handlers[action.type]?handlers[action.type](state, action): state;
+    console.log(JSON.stringify(action.type));
+    console.log(types);
+    switch (action.type) {
+        case types.FETCH_SATR_WARS_SUCCESS:
+            return {
+                ...state,
+                people:action.data
+            }
+        case types.FETCH_SATR_WARS_PLANET_SUCCESS:
+            return {
+                ...state,
+                planet: action.data
+            }
+        default: return initialState;
     }
-}
-const handleStarWarsPlanetFetchSuccess=(state, action)=>{
-    console.log(action.data);
-    return {
-        ...state,
-        planet: action.data
-    }
-}
-export default(state=initialState, action)=>{
-    const handlers={
-        [types.FETCH_SATR_WARS_SUCCESS]:handleStarWarsFetchSuccess,
-        [types.FETCH_SATR_WARS_PLANET_SUCCESS]:handleStarWarsPlanetFetchSuccess
-    }
-    return handlers[action.type]?handlers[action.type](state, action): state;
 }
