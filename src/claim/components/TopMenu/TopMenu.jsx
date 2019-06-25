65320783eb319e344fd092757fca5c7f95bc2ca7
iw <igpfpyu@vip.qq.com>
2019-06-25 10:21:01 +0800

    info

--- src/claim/components/TopMenu/TopMenu.jsx
+++ src/claim/components/TopMenu/TopMenu.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import {Layout, Menu} from 'antd';
+import {NavLink} from "react-router-dom";
+import './TopMenu.less';
+import NavList from './TopMenu_data';
 const { Header} = Layout;
 export default class TopMenu extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            current:"0"
+            current:"1010"
         }
     }
     render(){
         return (
             <Header className="top-header">
-                <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-                    <Menu.Item key="mail2"> Navigation One</Menu.Item>
+                <NavLink to="" className="logo">LOGO</NavLink>
+                <Menu onClick={(e)=>this.handleClick(e)}
+                      selectedKeys={[this.state.current]}
+                      mode="horizontal">
+                    {
+                        NavList.map((item, index) =><Menu.Item key={item.id}>{item.name}</Menu.Item>)
+                    }
                 </Menu>
             </Header>
         )
     }
-    handleClick(){
-
+    handleClick(e){
+            this.setState({
+                current:e.key
+            })
     }
 }
