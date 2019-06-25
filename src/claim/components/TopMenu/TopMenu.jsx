01d9ff0d047fa8560f9b56faff6f19223c859086
iw <igpfpyu@vip.qq.com>
2019-06-25 10:50:23 +0800

    info

--- src/claim/components/TopMenu/TopMenu.jsx
+++ src/claim/components/TopMenu/TopMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Layout, Menu} from 'antd';
+import {Layout, Menu, Button, Dropdown} from 'antd';
 import {NavLink} from "react-router-dom";
 import './TopMenu.less';
 import NavList from './TopMenu_data';
@@ -14,16 +14,37 @@ export default class TopMenu extends React.Component{
     render(){
         return (
             <Header className="top-header">
-                <NavLink to="" className="logo">LOGO</NavLink>
-                <Menu onClick={(e)=>this.handleClick(e)}
-                      selectedKeys={[this.state.current]}
-                      mode="horizontal">
-                    {
-                        NavList.map((item, index) =><Menu.Item key={item.id}>{item.name}</Menu.Item>)
-                    }
-                </Menu>
+                <div className="header-left">
+                    <NavLink to="" className="logo">LOGO</NavLink>
+                    <Menu onClick={(e)=>this.handleClick(e)}
+                          selectedKeys={[this.state.current]}
+                          mode="horizontal">
+                        {
+                            NavList.map((item, index) =><Menu.Item key={item.id}>{item.name}</Menu.Item>)
+                        }
+                    </Menu>
+                </div>
+                <div style={{float:"right"}}>
+                    <Dropdown overlay={this.dropdownMenu()} trigger={['click']}>
+                        <Button shape="circle" icon="user" size="small" />
+                    </Dropdown>
+                </div>
             </Header>
         )
+    }
+    dropdownMenu(){
+        return (
+            <Menu onClick={(e)=>this.dropdownMenuCLick(e)}>
+                <Menu.Item key="1">个人设置 </Menu.Item>
+                <Menu.Item key="2">退出</Menu.Item>
+            </Menu>
+        )
+    }
+    dropdownMenuCLick(e){
+        console.log(e)
+    }
+    handleMenuClick(){
+
     }
     handleClick(e){
             this.setState({
