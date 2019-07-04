dd5f4c105dbf1ccf38450dc537d0e38500bd9373
iw <igpfpyu@vip.qq.com>
2019-07-04 10:14:44 +0800

    info

--- src/claim/pages/Home/HomePage/HomePage_action.js
+++ src/claim/pages/Home/HomePage/HomePage_action.js
@@ -1,6 +1,8 @@
 import  * as types from '../HomePage_type';
 import {put, takeEvery} from 'redux-saga/effects';
+
 export const itemClick=()=>({type:types.ITEM_CLICK});
+
 function* itemCLick(){
     yield put({
         type:types.ITEM_CLICK_SUCCESS,
